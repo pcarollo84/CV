bbf02af0294d3057120d3c1b36ea7bd9e99ba0e5
Paolo Carollo <pcarollo84@gmail.com>
2018-03-04 10:27:15 +0000

    root model added

---
+++ CV/Models/Root.swift
@@ -0,0 +1,15 @@
+//
+//  Root.swift
+//  CV
+//
+//  Created by Paolo Carollo on 04/03/2018.
+//  Copyright © 2018 Paolo Carollo. All rights reserved.
+//
+
+import Foundation
+
+struct Root: Codable {
+    
+    var areas: [Area]
+    
+}
