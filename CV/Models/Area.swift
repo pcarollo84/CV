7cad625b8415e51fd80645b6a14f9d87ef514c4d
Paolo Carollo <pcarollo84@gmail.com>
2018-03-04 10:25:50 +0000

    area model added

---
+++ CV/Models/Area.swift
@@ -0,0 +1,16 @@
+//
+//  Area.swift
+//  CV
+//
+//  Created by Paolo Carollo on 04/03/2018.
+//  Copyright © 2018 Paolo Carollo. All rights reserved.
+//
+
+import Foundation
+
+struct Area: Codable {
+    
+    var title: String
+    var sections: [Section]
+    
+}
